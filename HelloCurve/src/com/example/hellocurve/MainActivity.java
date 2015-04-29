75dbcd3c9bca5f6c50cdc0da12801a9b2af08cf7
walfud <walfud@aliyun.com>
2015-04-29 14:31:20 +0800

    Fix `RadioGroup.check()` re-entry three times when change selection.

--- HelloCurve/src/com/example/hellocurve/MainActivity.java
+++ HelloCurve/src/com/example/hellocurve/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.hellocurve;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AnticipateInterpolator;
@@ -13,12 +12,20 @@ import android.view.animation.DecelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.OvershootInterpolator;
-import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements CompoundButton.OnCheckedChangeListener {
 
-	private RadioGroup mInterpolatorGroup = null;
+    private RadioButton mAccelerateDecelerate;
+    private RadioButton mAccelerateInterpolator;
+    private RadioButton mAnticipateInterpolator;
+    private RadioButton mAnticipateOvershoot;
+    private RadioButton mBounceInterpolator;
+    private RadioButton mCycleInterpolator;
+    private RadioButton mDecelerateInterpolator;
+    private RadioButton mLinearInterpolator;
+    private RadioButton mOvershootInterpolator;
 	private CurveView mCurveView = null;
 	private float[] mXValues = new float[] { 0.0f, 0.1f, 0.2f, 0.3f, 0.4f,
 			0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f };
@@ -28,42 +35,44 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		mInterpolatorGroup = (RadioGroup) findViewById(R.id.interpolatorGroup);
-		mCurveView = (CurveView) findViewById(R.id.curve);
+
+        mAccelerateDecelerate = (RadioButton) findViewById(R.id.AccelerateDecelerateInterpolator);
+        mAccelerateInterpolator = (RadioButton) findViewById(R.id.AccelerateInterpolator);
+        mAnticipateInterpolator = (RadioButton) findViewById(R.id.AnticipateInterpolator);
+        mAnticipateOvershoot = (RadioButton) findViewById(R.id.AnticipateOvershootInterpolator);
+        mBounceInterpolator = (RadioButton) findViewById(R.id.BounceInterpolator);
+        mCycleInterpolator = (RadioButton) findViewById(R.id.CycleInterpolator);
+        mDecelerateInterpolator = (RadioButton) findViewById(R.id.DecelerateInterpolator);
+        mLinearInterpolator = (RadioButton) findViewById(R.id.LinearInterpolator);
+        mOvershootInterpolator = (RadioButton) findViewById(R.id.OvershootInterpolator);
+        mCurveView = (CurveView) findViewById(R.id.curve);
+
+        //
+        mAccelerateDecelerate.setOnCheckedChangeListener(this);
+        mAccelerateInterpolator.setOnCheckedChangeListener(this);
+        mAnticipateInterpolator.setOnCheckedChangeListener(this);
+        mAnticipateOvershoot.setOnCheckedChangeListener(this);
+        mBounceInterpolator.setOnCheckedChangeListener(this);
+        mCycleInterpolator.setOnCheckedChangeListener(this);
+        mDecelerateInterpolator.setOnCheckedChangeListener(this);
+        mLinearInterpolator.setOnCheckedChangeListener(this);
+        mOvershootInterpolator.setOnCheckedChangeListener(this);
+
 		mCurveView.setxValues(mXValues);
-		mInterpolatorGroup
-				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
-					@Override
-					public void onCheckedChanged(RadioGroup group, int checkedId) {
-						Interpolator interpolator = getInterpolator();
-						for (int i = 0; i < mXValues.length; ++i) {
-							mYValues[i] = interpolator
-									.getInterpolation(mXValues[i]);
-						}
-						mCurveView.setyValues(mYValues);
-						mCurveView.invalidate();
-					}
-				});
-
-		mInterpolatorGroup.check(R.id.LinearInterpolator);
-
+        mLinearInterpolator.setChecked(true);
 	}
 
-	private Interpolator getInterpolator() {
-
-		int checkedId = mInterpolatorGroup.getCheckedRadioButtonId();
+	private Interpolator getInterpolator(int checkedId) {
 
 		switch (checkedId) {
 		case R.id.AccelerateDecelerateInterpolator:
 			return new AccelerateDecelerateInterpolator();
 
 		case R.id.AccelerateInterpolator:
-
 			return new AccelerateInterpolator();
 
 		case R.id.AnticipateInterpolator:
-
 			return new AnticipateInterpolator();
 
 		case R.id.AnticipateOvershootInterpolator:
@@ -73,11 +82,9 @@ public class MainActivity extends Activity {
 			return new BounceInterpolator();
 
 		case R.id.CycleInterpolator:
-
 			return new CycleInterpolator(2);
 
 		case R.id.DecelerateInterpolator:
-
 			return new DecelerateInterpolator();
 
 		case R.id.LinearInterpolator:
@@ -87,11 +94,23 @@ public class MainActivity extends Activity {
 			return new OvershootInterpolator();
 
 		default:
-			break;
+			return null;
 		}
-
-		return null;
 
 	}
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (!isChecked) {
+            return;
+        }
+
+        Interpolator interpolator = getInterpolator(buttonView.getId());
+        for (int i = 0; i < mXValues.length; ++i) {
+            mYValues[i] = interpolator
+                    .getInterpolation(mXValues[i]);
+        }
+        mCurveView.setyValues(mYValues);
+        mCurveView.invalidate();
+    }
 }
